a13a555224c9c0aa799c00d8396ac6706a549d53
Aditya <aditya.patro@expressstores.in>
2022-05-01 18:12:13 +0530

    adding compnent

---
+++ src/components/header/mobile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mobile() {
+  return (
+    <div>
+        this is Mobile
+    </div>
+  )
+}
+
+export default Mobile
---
+++ src/components/header/web/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './web.css';
+function Web() {
+  return (
+    <div className='web'>
+        <div className='web-option'>
+          <a href='#project'>
+          {/* <i class="fi fi-rs-edit-alt option-icon"></i>Projects */}
+
+          </a>
+        </div>
+        <div className='web-option'>
+          <a href='#skills'>
+
+          </a>
+        </div>
+        <div className='web-option'>
+          <a href='#work'>
+
+          </a>
+        </div>
+        <div className='web-option'>
+          <a href='#contact'>
+
+          </a>
+        </div>
+    </div>
+  )
+}
+
+export default Web
