c5820a1a78fc7201ff4fb14810945ef19bc271c2
Aditya <aditya.patro@expressstores.in>
2022-05-02 15:46:39 +0530

    body skills

--- src/components/body/skills/index.js
+++ src/components/body/skills/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import Seperator from '../../common/seperator';
+import './skills.css';
+import {SkillsData} from '../../../data/skills';
+import SkillCard from "./skill-card";
 function Skills() {
+  const data=SkillsData;
   return (
-    <div>this is Skills</div>
+    <div className='skills'>
+      <Seperator/>
+      <label className='section-title'>Skills</label>
+      <div className='skills-container'>
+        {data.map((item)=>{
+          return(
+          <div className='skills-section'>
+            <label className='skills-sectioin-title'>{item.type}</label>
+            <div className='skills-list'>
+              {item.list.map((skill)=>{
+                return(
+                  <SkillCard skill={skill}/>
+              )
+              })}
+
+
+            </div>
+          </div>  
+            )
+        })}
+      </div>
+    </div>
   )
 }
 
